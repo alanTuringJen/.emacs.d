55f02a93279d7e8d58f2fa721b2107087259e670
Alan <1649859811@qq.com>
2018-02-21 20:12:42 +0800

    Add files via upload

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -11,6 +10,7 @@
 (require 'init-ui)
 (require 'init-packages)
 (require 'init-shortcut-key)
+(require 'init-dired)
 
 (require 'recentf)
 (recentf-mode t)
@@ -28,7 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.1)
+ '(company-idle-delay 0.08)
  '(company-minimum-prefix-length 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
