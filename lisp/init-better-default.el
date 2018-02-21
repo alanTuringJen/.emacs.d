c50766a07c660aba7c3a910992b48b8423be225d
Alan <1649859811@qq.com>
2018-02-21 20:13:25 +0800

    Add files via upload

--- lisp/init-better-default.el
+++ lisp/init-better-default.el
@@ -1,8 +1,8 @@
-(global-auto-revert-mode t)
-(global-linum-mode 1)
-(setq make-backup-files nil) ;; 关闭自动备份文件
-(delete-selection-mode 1)
 (setq auto-save-default nil) ;; 关闭自动保存文件
+(global-auto-revert-mode t) ;; 自动加载外部修改过的文件
+(global-linum-mode t)
+(setq make-backup-files nil) ;; 关闭自动备份文件
+(delete-selection-mode t)
 (setq ring-bell-function 'ignore) ;;关闭警告音
 (fset 'yes-or-no-p 'y-or-n-p)
 ;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -15,7 +15,10 @@
 		company
 		swiper
 		counsel
-		spacemacs-theme
+		zenburn-theme
+		leuven-theme
+		smartparens
+		popwin
 		) "Default packages")
 (setq package-selected-packages my/packages)
 
@@ -31,19 +34,30 @@
         (when (not (package-installed-p pkg))
 	  (package-install pkg))))
 
+
 ;; Theme configuration
-(load-theme 'spacemacs-dark t)
+(load-theme 'zenburn t)
+
+;; Smartparens configuration
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; Popwin configuration
+(require 'popwin)
+(popwin-mode t)
 
 ;; Swiper configuration
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
 ;; Auto-complete configuration
 (global-company-mode 1)
+
 
 (provide 'init-packages)
