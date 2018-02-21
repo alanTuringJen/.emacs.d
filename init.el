94f762d2f172a15b79bd50f27b66e6310d851042
Alan <1649859811@qq.com>
2018-02-21 14:55:50 +0800

    Add files via upload

--- init.el
+++ init.el
@@ -7,11 +7,32 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
-(require 'init-packages)
-(require 'init-color-theme)
 (require 'init-better-default)
 (require 'init-ui)
+(require 'init-packages)
 (require 'init-shortcut-key)
+
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-menu-items 30)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
 
 (setq python-shell-interpreter "ipython"
      python-shell-interpreter-args "--simple-prompt -i")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
