4027d7146e887f9508f7258bd0c78d912c95c256
Alan <1649859811@qq.com>
2018-03-03 18:18:42 +0800

    Add files via upload

--- init.el
+++ init.el
@@ -11,26 +11,32 @@
 (require 'init-packages)
 (require 'init-shortcut-key)
 (require 'init-dired)
+(require 'init-org-mode)
 
+;; Recent files configuration
 (require 'recentf)
 (recentf-mode t)
 (setq recentf-max-menu-items 30)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
-
 (setq python-shell-interpreter "ipython"
-     python-shell-interpreter-args "--simple-prompt -i")
+	  python-shell-interpreter-args "--simple-prompt -i")
+
+;; Customize packages
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.08)
- '(company-minimum-prefix-length 2)
- '(which-key-idle-delay 0.08))
+ '(company-minimum-prefix-length 1)
+ '(package-selected-packages
+   (quote
+	(company which-key swiper counsel zenburn-theme smartparens popwin hungry-delete)))
+ '(which-key-idle-delay 0.15)
+ '(word-wrap nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
